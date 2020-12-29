29159e588f2f600ef2ef37e8ae70a0ccca2392e9
Bryan Rhodunda <rhodunda@udel.edu>
2020-12-28 23:42:45 -0500

    updated

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,21 @@
 
-const numbers = [1,2,3,4,5,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,50,150,1000,234,34,345,6554,676,786,5764,243444,5666,]
+const numbers = [6]
 
 
 const fizzBuzz = () => {
     for(let i = 0; i < numbers.length; i++) {
-        if(i / 3 == 0 && i / 6 == 0) {
+        if(i % 3 == 0 && i % 5 == 0) {
             console.log('fizz-buzz')
-        } if(i / 3 == 0) {
+           
+        } if(i % 3 == 0) {
             console.log('fizz')
-    }if(i / 6 == 0) {
+           
+    }if(i % 5 == 0) {
         console.log('buzz')
-    }if(i === undefined) {
-        console.log('number undefined')
+        
     }
 }
 }
-
+console.log(numbers)
 
 console.log(fizzBuzz(numbers))
