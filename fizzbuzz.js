ca6fe78ca94f00d09a8d9510ad77ee4c7c485852
Bryan Rhodunda <rhodunda@udel.edu>
2020-12-29 23:37:03 -0500

    updated fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,27 @@
 
-const numbers = [6]
+const numbers = [4]
 
+
+// const fizzBuzz = () => {
+//     for(let i = 0; i < numbers.length; i++) {
+//         if(i % 3 === 0 && i % 5 === 0) {
+//             console.log('fizz-buzz')
+           
+//         }  else if(i%3 === 0) {
+//             console.log('fizz')
+        
+//         } else if(i%5 === 0) {
+//         console.log('buzz')
+        
+//     }
+// }
+// }
 
 const fizzBuzz = () => {
-    for(let i = 0; i < numbers.length; i++) {
-        if(i % 3 == 0 && i % 5 == 0) {
-            console.log('fizz-buzz')
-           
-        } if(i % 3 == 0) {
-            console.log('fizz')
-           
-    }if(i % 5 == 0) {
-        console.log('buzz')
-        
+   
+    for(i = 0; i > numbers.length; i++) {
+
     }
-}
 }
 console.log(numbers)
 
