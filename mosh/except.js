23e12023c3ce8d4b92b58dd2fa62978e9e9f9321
Bryan Rhodunda <rhodunda@udel.edu>
2021-01-12 00:21:18 -0500

    added except

---
+++ mosh/except.js
@@ -0,0 +1,11 @@
+const arr = [10, 20, 30, 40, 50, 10, 30, 45]
+const ex = 10
+
+
+
+const except = (arr, ex) => {
+   const answer =  arr.filter(num => num !== ex)
+   return answer
+}
+
+console.log(`[except] ${except(arr, ex)}`)
