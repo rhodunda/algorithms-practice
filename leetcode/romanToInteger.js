0e18d61ce46b4313cb56a66dcb48fd64bf7c0453
Bryan Rhodunda <rhodunda@udel.edu>
2021-01-26 13:00:12 -0500

    finished I for roman to integer

--- leetcode/romanToInteger.js
+++ leetcode/romanToInteger.js
@@ -15,10 +15,29 @@
 // x can be place before a L and C to make 40 or 90
 // C can be place before a D and M to make 400 or 900
 
-const roman = IIXX
+const roman = "IIIV"
 
 
 
-const toInterger = (roman) => [
+const toInterger = (roman) => {
 
-]
+    let romanArray = roman.split("")
+    let count = 0
+
+    for(let i = 0; i < romanArray.length; i++) {
+        if(romanArray[i] === "V") {
+                if(romanArray[i - 1] === "I") {
+                    count += 3
+                }
+                if( romanArray[i + 1] === "I") {
+                    count += 6
+                    i++
+                }
+        }else count += 1
+    }
+    return count
+    
+}
+
+
+console.log(`[roman to interger] ${toInterger(roman)}`)
