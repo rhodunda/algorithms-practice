5e976b03081f238c853cd21731ea87510708ce06
Bryan Rhodunda <rhodunda@udel.edu>
2021-01-25 23:05:00 -0500

    updated palindrome Numbers

--- leetcode/palindromeNumbers.js
+++ leetcode/palindromeNumbers.js
@@ -6,5 +6,25 @@ const number = 121
 // else return false
 
 const isPalindrome = (num) => {
+console.log('-----')
+ let numString = JSON.stringify(num)
+let splitString = numString.split('')
+let reverseNum = splitString.reverse()
 
+let isTrue = 0
+for(let i = 0; i < splitString.length; i++){
+    for(let j = 0; j < reverseNum.length; j++) {
+        if(splitString[i] !== reverseNum[j]) {
+            isTrue += 1
+            console.log('test')
+            console.log(i, j)
+            console.log('----')
+        }
+    }
 }
+if(isTrue === 0 ? console.log(true) : console.log(false) )
+
+console.log('-----')
+}
+
+console.log(isPalindrome(number))
