55240e66065e7531bdc0000691490785b4f3add8
Bryan Rhodunda <rhodunda@udel.edu>
2021-01-26 00:05:05 -0500

    added roman rules

--- leetcode/romanToInteger.js
+++ leetcode/romanToInteger.js
@@ -1,7 +1,24 @@
 
+// convert the roman number symbols to numbers
+
+
+// symbol --- value
+//     I --- 1
+//     V --- 5
+//     X --- 10
+//     L --- 50
+//     C --- 100
+//     D --- 500
+//     M --- 1000
+
+// I can be place before a V and X to make 4 or 9
+// x can be place before a L and C to make 40 or 90
+// C can be place before a D and M to make 400 or 900
 
 const roman = IIXX
 
+
+
 const toInterger = (roman) => [
-    
+
 ]
