c777125ce310652fef6c6ee1f9dafe2a72da9180
Bryan Rhodunda <rhodunda@udel.edu>
2021-01-26 16:33:13 -0500

    added switch and case answer forroman to integer

--- leetcode/romanToInteger.js
+++ leetcode/romanToInteger.js
@@ -67,3 +67,70 @@ const toInterger = (roman) => {
 }
 
 console.log(`[roman to interger] ${toInterger(roman)}`)
+
+// Better switch and case answer I used for LeetCode
+
+
+
+
+// var romanToInt = function(s) {
+    
+//     const symbolArray = s.split("")
+//     let count = 0
+    
+//     for(let i = 0; i < symbolArray.length; i++ ) {
+        
+//         switch(symbolArray[i]){
+//             case "V":
+//                 if(symbolArray[i - 1] === "I")
+//                     count += 3
+                
+//                 else count += 5
+                
+//                 break;
+                
+//             case "X":
+//                 if(symbolArray[i - 1] === "I")
+//                     count += 8
+                
+//                 else count += 10
+                
+//                 break;
+            
+//             case "L":
+//                  if(symbolArray[i - 1] === "X")
+//                     count += 30
+                
+//                 else count += 50
+                
+//                 break;
+                
+//             case "C":
+//                  if(symbolArray[i - 1] === "X")
+//                     count += 80
+                
+//                 else count += 100
+                
+//                 break;
+                
+//             case "D":
+//                  if(symbolArray[i - 1] === "C")
+//                     count += 300
+                
+//                 else count += 500
+                
+//                 break;
+            
+//             case "M":
+//                  if(symbolArray[i - 1] === "C")
+//                     count += 800
+                
+//                 else count += 1000
+                
+//                 break;
+//             default:
+//                 count += 1
+//         }
+//     }
+//     return count
+// };
