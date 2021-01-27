ba68bc40d80e39df071c2d217f2a263c74cf6f63
Bryan Rhodunda <rhodunda@udel.edu>
2021-01-27 14:13:30 -0500

    added integer to roman"

---
+++ leetcode/integerToRoman.js
@@ -0,0 +1,30 @@
+// convert the integer given to roman numerables
+//
+// symbol --- value
+//     I --- 1
+//     V --- 5
+//     X --- 10
+//     L --- 50
+//     C --- 100
+//     D --- 500
+//     M --- 1000
+
+// I can be place before a V and X to make 4 or 9
+// x can be place before a L and C to make 40 or 90
+// C can be place before a D and M to make 400 or 900
+
+
+let integer = 30
+
+
+const ToRoman = (int) => {
+// example num 44
+// XXXIV
+if(int >= 10) {
+
+}
+}
+
+console.log('--------')
+console.log(ToRoman(integer))
+console.log('--------')
