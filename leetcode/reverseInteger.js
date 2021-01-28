d9f5199f0d5cfbfe46609d6f3e90b801b3dda354
Bryan Rhodunda <rhodunda@udel.edu>
2021-01-28 01:10:50 -0500

    added revered string with 32 bit cap answer

--- leetcode/reverseInteger.js
+++ leetcode/reverseInteger.js
@@ -1,14 +1,25 @@
 
 // reverse the integer
 
+// const integer = 123
+
+
+// function reverseInteger (int) {
+//     const reverse = int.toString().split('').reverse().join('')
+
+//     console.log(reverse)
+// }
+
+// reverseInteger(integer)
+
+
+
+// return the integer reversed 
+// if integer is over 32 bits return 0
 const integer = 123
 
-
-function reverseInteger (int) {
-    const reverse = int.toString().split('').reverse().join('')
-
-    console.log(reverse)
+const reversed = (int) => {
+    if(int < 0) return -1 * reverse(-int);
+    const reversedString = int.toString().split('').reverse().join('')
+    return (reverseString > 2**31 -1) ? 0 : reverseString
 }
-
-
-reverseInteger(integer)
