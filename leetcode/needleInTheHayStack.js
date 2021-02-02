fb0527a179fc889a6072a915c0db66f7e77fdab0
Bryan Rhodunda <rhodunda@udel.edu>
2021-02-02 18:52:50 -0500

    added instructions and started problem

--- leetcode/needleInTheHayStack.js
+++ leetcode/needleInTheHayStack.js
@@ -2,6 +2,16 @@
 const hayStack = 'apple'
 const needle = 'pp'
 
+// if neddle in found return 1
+// if empty return 0
+// if not found return -1
+
 const findNeedle = (hay, needle) => {
-    
+
+    if(hay === '' || needle === '') {
+        return 0
+    }
+    for(let i = 0; i < hay.length; i++) {
+        if
+    }
 }
