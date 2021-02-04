bf0eb317f0e0f97a1c728638d6e684c0c2daf36d
Bryan Rhodunda <rhodunda@udel.edu>
2021-02-03 23:43:22 -0500

    finished setup for pairs of socks

--- leetcode/needleInTheHayStack.js
+++ leetcode/needleInTheHayStack.js
@@ -13,11 +13,11 @@ const findNeedle = (hay, needle) => {
 
    if(needle === hay) return 0
 
-   for(let i = 0; i >= hay.length - needle.length; i++) {
+   for(let i = 0; i <= hay.length - needle.length; i++) {
        // did hay.length - needle.length because its not possable after that point
 
        if(needle === hay.substring(i, i+needle.length)) {
            return i
-       } else return -1
-   }
+       } 
+   }return -1
 }
---
+++ leetcode/pairsOfSocks.js
@@ -0,0 +1,11 @@
+
+// return number of pairs
+
+const sockArray = [1,2,4,1,2,3,2]
+
+
+const findPairs = (arr) => {
+
+}
+
+console.log(findPairs(sockArray))
