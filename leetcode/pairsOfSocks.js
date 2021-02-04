b8a457f88adf5b088e073b7463d393d67ca4aaec
Bryan Rhodunda <rhodunda@udel.edu>
2021-02-03 23:51:57 -0500

    finished sock pairs

--- leetcode/pairsOfSocks.js
+++ leetcode/pairsOfSocks.js
@@ -1,11 +1,21 @@
 
 // return number of pairs
 
-const sockArray = [1,2,4,1,2,3,2]
-
+const sockArray = [1,2,4,1,2,3,2,3,2]
+               
 
 const findPairs = (arr) => {
+let pairs = 0
 
+const sortedSockArray = arr.sort()
+
+for(let i = 0; i < sortedSockArray.length; i++) {
+    if(sortedSockArray[i] === sortedSockArray[i + 1]) {
+        pairs += 1
+        i++
+    }
+}
+return pairs
 }
 
 console.log(findPairs(sockArray))
