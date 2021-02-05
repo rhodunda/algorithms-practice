0e120b1d5b4395edc464be16464e956045272ee0
Bryan Rhodunda <rhodunda@udel.edu>
2021-02-04 19:11:26 -0500

    started longest substring

---
+++ leetcode/longestSubString.js
@@ -0,0 +1,14 @@
+
+const subString = 'abcafkefo'
+
+
+const longestSubstring = (string) => {
+    let set = new set()
+
+    let left = 0
+    let right = 0
+
+    let count = 0
+
+    
+    }
--- leetcode/pairsOfSocks.js
+++ leetcode/pairsOfSocks.js
@@ -2,7 +2,7 @@
 // return number of pairs
 
 const sockArray = [1,2,4,1,2,3,2,3,2]
-               
+
 
 const findPairs = (arr) => {
 let pairs = 0
