4ac8da9cf6eed1286a2adc744a99589cd4c9f2bd
Bryan Rhodunda <rhodunda@udel.edu>
2021-02-08 17:42:53 -0500

    added stacks funtional way

--- StacksAndQueues.js
+++ StacksAndQueues.js
@@ -12,3 +12,39 @@ stack.pop()
 
 // peek
 stack[stack.length - 1]
+
+// stack with classes
+
+class Stack {
+    constructor() {
+        this.storage = {}
+        this.size = 0
+    }
+
+    push(element) {
+        this.size++
+        this.storage[this.size] = element
+    }
+
+    pop() {
+            let removed = this.storage[this.size]
+            delete this.storage[this.size]
+            this.size--
+            return removed
+    }
+
+    peek() {
+        this.storage[this.size]
+    }
+}
+
+
+const paper = new Stack()
+paper.push('2')
+paper.push('1')
+
+paper.pop('2')
+
+paper.peek()
+
+console.log(paper)
