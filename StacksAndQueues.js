1ccf7647a0cde6cc5f2672e3af9b4fe8e11c4748
Bryan Rhodunda <rhodunda@udel.edu>
2021-02-08 17:27:10 -0500

    added stack

---
+++ StacksAndQueues.js
@@ -0,0 +1,14 @@
+// stack
+
+let stack = []
+
+// push to stack
+stack.push("cat")
+stack.push("dog")
+stack.push('bear')
+
+// pop  // removes first element from stack
+stack.pop()
+
+// peek
+stack[stack.length - 1]
