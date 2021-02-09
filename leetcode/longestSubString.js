68ec7858dfe144d2746dc376239fa963d31e1f32
Bryan Rhodunda <rhodunda@udel.edu>
2021-02-09 16:42:03 -0500

    updated longest sub string

--- leetcode/longestSubString.js
+++ leetcode/longestSubString.js
@@ -3,12 +3,23 @@ const subString = 'abcafkefo'
 
 
 const longestSubstring = (string) => {
-    let set = new set()
-
-    let left = 0
-    let right = 0
-
-    let count = 0
-
+    let set = new Set(),
+        count = 0,
+        arrayString = string.split('')
+    
+    
+    for(let i = 0; i < arrayString.length; i++) {
+       
+            if(set.has(i))  {
+                return count
+            }
+            else {
+                set.add(i)
+                count ++
+            }
+        
+    }
     
     }
+
+    console.log(longestSubstring(subString))
